ceb4bc1ee1edb20709648e00256682e06e36770e
蔡相襄 <78093303+hsiang2@users.noreply.github.com>
2023-05-18 14:23:55 +0800

    MyWebSite

---
+++ boardapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from captcha.fields import CaptchaField
+
+# Create your models here.
+class  BoardUnit(models.Model):
+    bname = models.CharField(max_length=20, null=False)
+    # bgender = models.BooleanField()
+    bgender = models.CharField(max_length=2, default='F', null=False)
+    btitle = models.CharField(max_length=100, null=False)
+    bcontent = models.TextField(null=False)
+    btime = models.DateTimeField(auto_now=True)
+    bemail = modelsEmailField(max_length=100, blank=True, default='')
+    bresponse = models.TextField(blank=True, default='')
+    
+
+    def __str__(self):
+        return self.btitle
